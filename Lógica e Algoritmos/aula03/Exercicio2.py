a86a6e21670ff643bc1a2f9e7524ca3e2b5fbfb9
ralflima <ralf.lima@gmail.com>
2024-03-04 21:25:48 -0300

    Terceira aula de Python

---
+++ Lógica e Algoritmos/aula03/Exercicio2.py
@@ -0,0 +1,20 @@
+# Obter número inicial
+print("Informe o número inicial")
+inicio = int(input())
+
+# Obter número final
+print("Informe o número final")
+termino = int(input())
+
+# Laço For
+for indice in range(inicio, termino+1):
+    print(indice)
+
+# Divisão
+print("-----------------")
+
+# Laço While
+while inicio <= termino:
+    print(inicio)
+    inicio+=1
+
